4a9f32d17d4bc2368e5e5979868d2196599e38d1
María José Valverde Peña <mariajosevalverdepena@gmail.com>
2023-02-05 22:01:11 -0600

    Primer commit práctica #2 desarrollo de aplicaciones web

---
+++ src/main/java/com/rubikscube/RubiksCubePage.java
@@ -0,0 +1,13 @@
+package com.rubikscube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiksCubePage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiksCubePage.class, args);
+	}
+
+}
